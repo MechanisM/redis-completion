575ca90ee61c3f3dbae9db9efe6ab6d54783c246
Charles Leifer <coleifer@gmail.com>
2012-06-07 10:43:25 -0500

    Bamp

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redis-completion',
-    version="0.1.2",
+    version="0.2.0",
     description='autocomplete with redis',
     author='Charles Leifer',
     author_email='coleifer@gmail.com',
